966de278ee65b218aaec28ccc5e1437838c2f960
Alistair Arcilla <alistair.arcilla@outlook.com>
2015-06-05 16:14:13 +1000

    Implemented basic viewing of user's Purchase history

---
+++ Bookland/Controllers/PurchasesController.cs
@@ -0,0 +1,29 @@
+﻿using Bookland.DAL.Abstract;
+using Bookland.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Bookland.Controllers
+{
+    [Authorize]
+    public class PurchasesController : Controller
+    {
+        IPurchaseRepository purchaseRepo;
+
+        public PurchasesController(IPurchaseRepository purchaseRepo)
+        {
+            this.purchaseRepo = purchaseRepo;
+        }
+
+        public ActionResult Index()
+        {
+            IEnumerable<Purchase> userPurchases = purchaseRepo.GetPurchases(User.Identity.Name);
+
+            if (userPurchases != null)
+                userPurchases = userPurchases.OrderByDescending(p => p.PurchaseDate);
+
+            return View(userPurchases);
+        }
+    }
+}
