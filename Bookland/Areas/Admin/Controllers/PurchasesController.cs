efb3fe509a6864acea1b1c03c08c598a116f3bac
Alistair Arcilla <alistair.arcilla@outlook.com>
2015-06-05 16:59:46 +1000

    Implemented viewing of Purchases within Admin section

---
+++ Bookland/Areas/Admin/Controllers/PurchasesController.cs
@@ -0,0 +1,36 @@
+﻿using Bookland.Areas.Admin.Models;
+using Bookland.DAL.Abstract;
+using Bookland.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Bookland.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Administrator, Support, Staff")]
+    public class PurchasesController : Controller
+    {
+        IPurchaseRepository purchaseRepo;
+
+        public PurchasesController(IPurchaseRepository purchaseRepo)
+        {
+            this.purchaseRepo = purchaseRepo;
+        }
+
+        public ActionResult Index(string userName)
+        {
+            IEnumerable<Purchase> userPurchases = purchaseRepo.GetPurchases(userName);
+
+            if (userPurchases != null)
+                userPurchases = userPurchases.OrderByDescending(p => p.PurchaseDate);
+
+            return View(new PurchasesViewModel 
+            {
+                Purchases = userPurchases,
+                UserName = userName
+            });
+        }
+
+    }
+}
---
+++ Bookland/Areas/Admin/Models/PurchasesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Bookland.Models;
+using System.Collections.Generic;
+
+namespace Bookland.Areas.Admin.Models
+{
+    public class PurchasesViewModel
+    {
+        public IEnumerable<Purchase> Purchases { get; set; }
+        public string UserName { get; set; }
+    }
+}
