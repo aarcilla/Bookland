f3251b641e84b725e496515a2be938c39185118a
Alistair Arcilla <alistair.arcilla@outlook.com>
2015-07-17 05:49:39 +1000

    Updated category-filtered Home page with heading that shows the chosen category's ancestor categories

--- Bookland/Controllers/HomeController.cs
+++ Bookland/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using Bookland.Helpers.Abstract;
 using Bookland.Models;
 using System;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -32,12 +33,29 @@ namespace Bookland.Controllers
             // Retrieve Category tree (a Category and its descendant Categories) for Category-filtered display
             TreeNode<Category> categoryTree = category.HasValue ? categoryRepo.GetCategoryTree(category.Value) : null;
 
+            // Generate category filter heading by prepending parent category names
+            string categoryHeading = string.Empty;
+            if (categoryTree != null) 
+            {
+                StringBuilder categoryHeadingBuilder = new StringBuilder(categoryTree.Data.CategoryName);
+                Category currentParentCategory = categoryRepo.GetParentCategory(categoryTree.Data.CategoryID);
+                while (currentParentCategory != null && !currentParentCategory.CategoryName.Equals("ROOT"))
+                {
+                    categoryHeadingBuilder.Insert(0, " > ");
+                    categoryHeadingBuilder.Insert(0, currentParentCategory.CategoryName);
+
+                    currentParentCategory = categoryRepo.GetParentCategory(currentParentCategory.CategoryID);
+                }
+
+                categoryHeading = categoryHeadingBuilder.ToString();
+            }
+
             return View(new ProductsViewModel
             {
                 Products = productHelpers.ProductsByOrder(productRepo, order, categoryTree, 
                                                             p => p.ProductStatus.ProductStatusAvailable).ToList<Product>(),
                 NumColumns = 3,
-                Heading = categoryTree != null ? categoryTree.Data.CategoryName : null,
+                Heading = categoryHeading,
                 OrderOptions = productHelpers.ProductOrderOptionsSelectList(order)
             });
         }
